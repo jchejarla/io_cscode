34c085b34b9eec8dd379c5f9998b8ed88f0da6d4
Janardhan Chejarla <j.chejarla@yahoo.com>
2021-12-31 13:35:11 -0500

    Quicksort implementation using Hoare's partition scheme

---
+++ src/main/java/io/cscode/algorithms/sorting/QuicksortHoarePartition.java
@@ -0,0 +1,52 @@
+package io.cscode.algorithms.sorting;
+
+import java.util.Arrays;
+
+public class QuicksortHoarePartition {
+
+    public void sortArray(int[] nums) {
+        if(nums == null || nums.length==0 ||nums.length==1) {
+            return;
+        }
+        quickSort(nums, 0, nums.length-1);
+    }
+
+    private void quickSort(int[] nums, int left, int right) {
+        if(left < right) {
+            int pivotIndex = partition(nums, left, right);
+            quickSort(nums, left, pivotIndex);
+            quickSort(nums, pivotIndex +1, right);
+        }
+    }
+    private int partition(int[] nums, int left, int right) {
+        int pivotIndex = left + (right-left)/2;
+        int pivot = nums[pivotIndex];
+
+        int i = left-1;
+        int j = right+1;
+
+        while (true) {
+
+            do {
+                i++;
+            } while(nums[i] < pivot);
+
+            do {
+                j--;
+            } while (nums[j] > pivot);
+
+            if(i >= j) {
+                return j;
+            }
+
+            swap(nums, i, j);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+}
--- src/test/java/io/cscode/algorithms/sorting/TestQuicksortHoarePartition.java
+++ src/test/java/io/cscode/algorithms/sorting/TestQuicksortHoarePartition.java
@@ -9,35 +9,36 @@ import java.util.Arrays;
 
 import static org.junit.Assert.assertTrue;
 
-public class TestQuicksortLomutoPartition extends UnitTestBase {
+public class TestQuicksortHoarePartition extends UnitTestBase {
 
     @Mock(answer = Answers.CALLS_REAL_METHODS)
-    QuicksortLomutoPartition quicksortLomutoPartition;
+    QuicksortHoarePartition quicksortHoarePartition;
 
     @Test
     public void testQuickSort() {
         int[] input = {3, 2, 1, 9, 8, 5, 6};
-        quicksortLomutoPartition.sortArray(input);
+        quicksortHoarePartition.sortArray(input);
         assertTrue(Arrays.equals(new int[]{1,2,3,5,6,8,9}, input));
     }
+
     @Test
     public void testQuickSortWithDuplicateElements() {
         int[] input = {3,3,2,1,2,9,8,4,6,22,11,5};
-        quicksortLomutoPartition.sortArray(input);
+        quicksortHoarePartition.sortArray(input);
         assertTrue(Arrays.equals(new int[]{1, 2, 2, 3, 3, 4, 5, 6, 8, 9, 11, 22}, input));
     }
 
     @Test
     public void testQuickSortWhenInputIsNull() {
         int[] input = null;
-        quicksortLomutoPartition.sortArray(input);
+        quicksortHoarePartition.sortArray(input);
         assertTrue(Arrays.equals(null, input));
     }
 
     @Test
     public void testQuickSortWhenInputIsEmpty() {
         int[] input = {};
-        quicksortLomutoPartition.sortArray(input);
+        quicksortHoarePartition.sortArray(input);
         assertTrue(0 == input.length);
     }
 }
