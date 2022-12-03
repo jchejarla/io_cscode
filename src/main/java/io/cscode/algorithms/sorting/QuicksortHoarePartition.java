7ec35d16ed314c31dc76761c7a812636456362b6
Janardhan Chejarla <j.chejarla@yahoo.com>
2022-12-03 10:08:30 -0500

    Adding comments to Quick sort algorithm implementations

--- src/main/java/io/cscode/algorithms/sorting/QuicksortHoarePartition.java
+++ src/main/java/io/cscode/algorithms/sorting/QuicksortHoarePartition.java
@@ -2,6 +2,11 @@ package io.cscode.algorithms.sorting;
 
 public class QuicksortHoarePartition {
 
+    /**
+     * Entry method this sorting algorithm
+     *
+     * @param nums input array
+     */
     public void sortArray(int[] nums) {
         if(nums == null || nums.length==0 ||nums.length==1) {
             return;
@@ -9,6 +14,13 @@ public class QuicksortHoarePartition {
         quickSort(nums, 0, nums.length-1);
     }
 
+    /**
+     * utility method, that calls partition method and recursively calls left and right sub arrays aplit at pivotIndex
+     *
+     * @param nums input array
+     * @param left left index
+     * @param right right index
+     */
     private void quickSort(int[] nums, int left, int right) {
         if(left < right) {
             int pivotIndex = partition(nums, left, right);
@@ -16,6 +28,17 @@ public class QuicksortHoarePartition {
             quickSort(nums, pivotIndex +1, right);
         }
     }
+
+    /**
+     * select the 'pivot' element and move elements which are lesser than pivot to left side of pivot
+     * and greater than pivot to right side of pivot.
+     * By the end of this, pivot is positioned in its position where it should be after array is sorted.
+     *
+     * @param nums
+     * @param left
+     * @param right
+     * @return pivot index where it is finally positioned
+     */
     private int partition(int[] nums, int left, int right) {
         int pivotIndex = left + (right-left)/2;
         int pivot = nums[pivotIndex];
@@ -41,6 +64,13 @@ public class QuicksortHoarePartition {
         }
     }
 
+    /**
+     * Utility method to swap elements
+     *
+     * @param nums input array
+     * @param i index to be swapped with j
+     * @param j index to be swapped with i
+     */
     private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
--- src/main/java/io/cscode/algorithms/sorting/QuicksortLomutoPartition.java
+++ src/main/java/io/cscode/algorithms/sorting/QuicksortLomutoPartition.java
@@ -2,6 +2,11 @@ package io.cscode.algorithms.sorting;
 
 public class QuicksortLomutoPartition {
 
+    /**
+     * Entry method this sorting algorithm
+     *
+     * @param nums
+     */
     public void sortArray(int[] nums) {
         if(nums == null || nums.length==0 ||nums.length==1) {
             return;
@@ -9,6 +14,13 @@ public class QuicksortLomutoPartition {
         quickSort(nums,0,nums.length-1);
     }
 
+    /**
+     * utility method, that calls partition method and recursively calls left and right sub arrays aplit at pivotIndex
+     *
+     * @param nums input array
+     * @param left left index
+     * @param right right index
+     */
     private void quickSort(int nums[], int left, int right) {
         if (left < right) {
             int partition = partition(nums, left, right);
@@ -17,6 +29,16 @@ public class QuicksortLomutoPartition {
         }
     }
 
+    /**
+     * select the 'pivot' element and move elements which are lesser than pivot to left side of pivot
+     * and greater than pivot to right side of pivot.
+     * By the end of this, pivot is positioned in its position where it should be after array is sorted.
+     *
+     * @param nums
+     * @param left
+     * @param right
+     * @return pivot index where it is finally positioned
+     */
     private int partition(int nums[], int left, int right) {
         int pivot = nums[right];
         int storeIndex = left;
@@ -29,6 +51,14 @@ public class QuicksortLomutoPartition {
         swap(nums, storeIndex, right); //at the end move pivot element (right indexed element) to its position to complete the partitioning.
         return storeIndex;
     }
+
+    /**
+     * Utility method to swap elements
+     *
+     * @param nums input array
+     * @param i index to be swapped with j
+     * @param j index to be swapped with i
+     */
     private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
