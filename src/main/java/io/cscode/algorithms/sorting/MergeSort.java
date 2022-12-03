3b1530c59c0654cd12afe96e133bba11a5a82d9a
Janardhan Chejarla <j.chejarla@yahoo.com>
2022-12-02 23:11:01 -0500

    Following changes: 1) Adding comments to MergeSort implementation 2) Bootstrapping web server using spring boot example

--- src/main/java/io/cscode/algorithms/sorting/MergeSort.java
+++ src/main/java/io/cscode/algorithms/sorting/MergeSort.java
@@ -1,7 +1,15 @@
 package io.cscode.algorithms.sorting;
 
+/**
+ * MergeSort implementation
+ */
 public class MergeSort {
 
+    /**
+     * Entry method this sorting algorithm
+     *
+     * @param nums input array
+     */
     public void sort(int[] nums) {
         if(nums == null || nums.length==0 ||nums.length==1) {
             return;
@@ -9,32 +17,49 @@ public class MergeSort {
         mergeSort(nums, 0, nums.length-1);
     }
 
+    /**
+     * Main function that divides array into two halves and recursively calls itself on sub arrays
+     * when the array cannot be split further, it calls merge() method to start merging on sub arrays.
+     *
+     * @param nums input array
+     * @param left start index
+     * @param right end index
+     */
     private void mergeSort(int[] nums, int left, int right) {
         if(left < right) {
             int mid = left + (right-left)/2;
-            mergeSort(nums, left, mid);
-            mergeSort(nums, mid+1, right);
-            merge(nums, left, mid, right);
+            mergeSort(nums, left, mid); //sort from 1...n/2
+            mergeSort(nums, mid+1, right); //sort from (n/2)+1 ...n
+            merge(nums, left, mid, right); //call merge function when input array can't be split be further
         }
     }
 
+    /**
+     * merge function to merge two sub arrays in sorting order.
+     *
+     * @param nums input array
+     * @param left left index
+     * @param mid mid index
+     * @param right right index
+     */
     private void merge(int[] nums, int left, int mid, int right) {
         int n1= mid -left +1; // mid inclusive and indexes starts with 0, so +1
         int n2 = right - mid;
 
-        int[] leftArray = new int[n1];
-        int[] rightArray = new int[n2];
+        int[] leftArray = new int[n1]; // left sub array
+        int[] rightArray = new int[n2]; // right sub array
 
         for (int i=0; i<n1; i++) {
-            leftArray[i] = nums[left+i];
+            leftArray[i] = nums[left+i]; // copy from left to mid onto left sub array
         }
 
         for (int j=0; j<n2; j++) {
-            rightArray[j] = nums[mid+1+j];
+            rightArray[j] = nums[mid+1+j]; // copy from mid+1 to right onto right sub array
         }
 
         int i=0; // counter to keep track of index for leftArray
         int j=0; // counter to keep track of index for rightArray
+
         for (int k=left; k <= right; k++) {
             if(i<leftArray.length && j<rightArray.length) {
                 if(leftArray[i] <=rightArray[j]) {
@@ -56,7 +81,7 @@ public class MergeSort {
                 }
             }
         }
+
     }
 
 }
-
---
+++ src/main/java/io/cscode/springboot/web/BootstrapWebServer.java
@@ -0,0 +1,12 @@
+package io.cscode.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapWebServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapWebServer.class, args);
+    }
+}
