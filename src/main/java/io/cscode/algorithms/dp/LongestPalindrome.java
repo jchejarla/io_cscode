47c0241d3d1b2c1ec9b55af5351b3694fac0994e
Janardhan Chejarla <j.chejarla@yahoo.com>
2023-10-13 15:59:01 -0400

    Longest Palindrome

--- src/main/java/io/cscode/algorithms/dp/LongestPalindrome.java
+++ src/main/java/io/cscode/algorithms/dp/LongestPalindrome.java
@@ -4,11 +4,11 @@ public class LongestPalindrome {
 
     /* brute force approach*/
     static String bruteForceApproach(String s) {
-        String longestPalindrome ="";
-        for(int i=0; i< s.length(); i++) {
-            for(int j=i; j<=s.length(); j++) { // we are doing j<=str.length() because substring method we are using below endIndex is exclusive
-                String sub = s.substring(i,j);
-                if(isPalindrome(sub) && sub.length()>longestPalindrome.length()) {
+        String longestPalindrome = "";
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j <= s.length(); j++) { // we are doing j<=str.length() because substring method we are using below endIndex is exclusive
+                String sub = s.substring(i, j);
+                if (isPalindrome(sub) && sub.length() > longestPalindrome.length()) {
                     longestPalindrome = sub;
                 }
             }
@@ -17,34 +17,34 @@ public class LongestPalindrome {
     }
 
     static boolean isPalindrome(String str) {
-        if(str == null || str.length() ==0) {
+        if (str == null || str.length() == 0) {
             return false;
         }
-        if(str.length() ==1) { // if the string length is 1, it is a palindrome
+        if (str.length() == 1) { // if the string length is 1, it is a palindrome
             return true;
         }
 
-        int i=0;
-        int j=0;
+        int i = 0;
+        int j = 0;
         int length = str.length();
 
-        if(length % 2 == 0){    // when string length is even, for example "abba" it's a palindrome with no center
-            int mid = length /2; // for "abba" mid will become 2, so we should set i=1 and j=2 (i.e. b b)
-                                // then, we can check remaining characters by moving i and j to left side and right side
-                                // of the string respectively
-            j=mid;
-            i=mid-1;
+        if (length % 2 == 0) {    // when string length is even, for example "abba" it's a palindrome with no center
+            int mid = length / 2; // for "abba" mid will become 2, so we should set i=1 and j=2 (i.e. b b)
+            // then, we can check remaining characters by moving i and j to left side and right side
+            // of the string respectively
+            j = mid;
+            i = mid - 1;
         } else {                // when string length is odd, for example "abcba" it's a palindrome with c as center
-            int mid = length /2; // for "abcba" mid will become 2, that is already center letter c
-                                 // so we should set i=1 and j=3 (i.e. b b)
-                                // then, we can check remaining characters by moving i and j to left side and right side
-                                // of the string respectively
-            j= mid+1;
-            i = mid-1;
+            int mid = length / 2; // for "abcba" mid will become 2, that is already center letter c
+            // so we should set i=1 and j=3 (i.e. b b)
+            // then, we can check remaining characters by moving i and j to left side and right side
+            // of the string respectively
+            j = mid + 1;
+            i = mid - 1;
         }
 
-        while (i>=0 && j<str.length()){
-            if(str.charAt(i) == str.charAt(j)) {
+        while (i >= 0 && j < str.length()) {
+            if (str.charAt(i) == str.charAt(j)) {
                 i--;
                 j++;
             } else {
@@ -58,38 +58,30 @@ public class LongestPalindrome {
     /* expand around center*/
     static String expandAroundCenter(String s) {
         String maxPalindrome = "";
-        for(int i=0; i<s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
 
             // this will check only odd length palindromes, because l and r pointing to same index i
-            int l = i;
-            int r = i;
-            while(l>=0 && r<s.length()) {
-                if(s.charAt(l) == s.charAt(r)) {
-                    if(r-l+1 >maxPalindrome.length()){
-                        maxPalindrome = s.substring(l, r+1);
-                    }
-                    l--;
-                    r++;
-                } else {
-                    break;
-                }
-            }
-
+            String maxPalindromeOdd = expandAroundCenterHelper(s, i, i);
             // since we have checked odd length palindromes above, we will repeat the same process for even length palindromes
-
-            l = i;
-            r = i+1;
-            while(l>=0 && r<s.length()) {
-                if(s.charAt(l) == s.charAt(r)) {
-                    if(r-l+1 >maxPalindrome.length()){
-                        maxPalindrome = s.substring(l, r+1);
-                    }
-                    l--;
-                    r++;
-                } else {
-                    break;
-                }
+            String maxPalindromeEven = expandAroundCenterHelper(s, i, i + 1);
+            if (maxPalindromeOdd.length() > maxPalindrome.length()) {
+                maxPalindrome = maxPalindromeOdd;
             }
+            if (maxPalindromeEven.length() > maxPalindrome.length()) {
+                maxPalindrome = maxPalindromeEven;
+            }
+        }
+        return maxPalindrome;
+    }
+
+    static String expandAroundCenterHelper(String s, int l, int r) {
+        String maxPalindrome = "";
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            if (r - l + 1 > maxPalindrome.length()) {
+                maxPalindrome = s.substring(l, r + 1);
+            }
+            l--;
+            r++;
         }
         return maxPalindrome;
     }
