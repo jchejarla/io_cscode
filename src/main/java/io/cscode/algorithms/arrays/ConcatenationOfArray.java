be8ab342eeffbf860e47a776399483a10d158308
Janardhan Chejarla <j.chejarla@yahoo.com>
2023-10-21 20:58:25 -0400

    Concatenation of Array

---
+++ src/main/java/io/cscode/algorithms/arrays/ConcatenationOfArray.java
@@ -0,0 +1,19 @@
+package io.cscode.algorithms.arrays;
+
+import java.util.Arrays;
+
+public class ConcatenationOfArray {
+
+    static int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length *2];
+        for(int i=0;i<nums.length; i++) {
+            ans[i] = nums[i];
+            ans[i+nums.length] = nums[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(getConcatenation(new int[]{1, 2, 1})));
+    }
+}
