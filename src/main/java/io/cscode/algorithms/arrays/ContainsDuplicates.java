ada3a2b4db1e7f95cf85090e528941ecd3e0d6c9
Janardhan Chejarla <j.chejarla@yahoo.com>
2023-12-24 11:51:51 -0500

    Update to contains duplicates

--- src/main/java/io/cscode/algorithms/arrays/ContainsDuplicates.java
+++ src/main/java/io/cscode/algorithms/arrays/ContainsDuplicates.java
@@ -1,5 +1,6 @@
 package io.cscode.algorithms.arrays;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class ContainsDuplicates {
@@ -12,6 +13,17 @@ public class ContainsDuplicates {
                 if(nums[i] == nums[j]) {
                     return true;
                 }
+            }
+        }
+        return false;
+    }
+
+    /* using sorting approach */
+    static boolean usingSortingApproach(int[] nums) {
+        Arrays.sort(nums); // O(n log n)
+        for(int i=1; i<nums.length; i++) {
+            if(nums[i] == nums[i-1]) {
+                return true;
             }
         }
         return false;
@@ -33,6 +45,10 @@ public class ContainsDuplicates {
         System.out.println("############### Brute force approach######################");
         System.out.println(bruteForceApproach(new int[] {1, 2, 3, 1}));
         System.out.println(bruteForceApproach(new int[] {1, 2, 3, 4}));
+
+        System.out.println("############### Using sorting approach######################");
+        System.out.println(usingSortingApproach(new int[] {1, 2, 3, 1}));
+        System.out.println(usingSortingApproach(new int[] {1, 2, 3, 4}));
 
         System.out.println("############### Using HashSet efficient approach######################");
         System.out.println(usingHashSet(new int[] {1, 2, 3, 1}));
