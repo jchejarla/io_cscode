17ec4575d91ab679a52f543bd5020eec1f6ce1b0
Janardhan Chejarla <j.chejarla@yahoo.com>
2023-12-29 11:35:09 -0500

    3 Sum

--- src/main/java/io/cscode/algorithms/arrays/ThreeSum.java
+++ src/main/java/io/cscode/algorithms/arrays/ThreeSum.java
@@ -10,7 +10,7 @@ public class ThreeSum {
         for(int i=0; i<nums.length; i++) {
             int num = nums[i];
             int twoSum = 0-num;
-            int[] twoSumIndexes = new TwoSum().twoSum(nums, twoSum);
+            int[] twoSumIndexes = TwoSum.twoSum(nums, twoSum);
             if(twoSumIndexes != null) {
                 List<Integer> triplet = Arrays.asList(num, nums[twoSumIndexes[0]], nums[twoSumIndexes[1]]);
                 triplet.sort(Comparator.naturalOrder()); // we can assume this is O(1) time as we are sorting just 3 numbers
@@ -27,7 +27,7 @@ public class ThreeSum {
         Arrays.sort(nums);// n (log n)
         List<List<Integer>> result = new ArrayList<>();
 
-        for (int i=0; i<nums.length; i++) {
+        for (int i=0; i<nums.length-2; i++) {
 
             if(i==0 || nums[i] != nums[i-1]) { // since the array is sorted, we can ignore elements i and i-1 if they are same.
                                                // reason for that is, if there is a triplet found at i, the same triplet would have been found at i-1 as well
@@ -63,5 +63,7 @@ public class ThreeSum {
     public static void main(String[] args) {
         System.out.println(new ThreeSum().threeSum_Using_2Sum(new int[]{-1, 0, 1, 2, -1, -4}));
         System.out.println(new ThreeSum().threeSum_Using_Sorting_And_2Sum(new int[]{-1, 0, 1, 2, -1, -4}));
+        System.out.println(new ThreeSum().threeSum_Using_Sorting_And_2Sum(new int[]{2,2,0,-2}));
+
     }
 }
