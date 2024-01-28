62a054d387e1c66f5062f311cd86958835df17e0
Janardhan Chejarla <j.chejarla@yahoo.com>
2024-01-27 22:07:36 -0500

    Permutation in string

--- src/main/java/io/cscode/algorithms/slidingwindow/PermutationInString.java
+++ src/main/java/io/cscode/algorithms/slidingwindow/PermutationInString.java
@@ -88,6 +88,60 @@ public class PermutationInString {
         return false;
     }
 
+
+    /**
+     * Similar to previous approaches, time complexity is O ( 26 *n). We can reduce it to just O(n)
+     * @param s1
+     * @param s2
+     * @return
+     */
+    static boolean checkInclusionUsingArraysAndMatchCount(String s1, String s2) {
+        if(s1.length() > s2.length()) {
+            return false;
+        }
+        int[] s1_count = new int[26];
+        int[] s2_count = new int[26];
+
+        for(int i=0; i<s1.length(); i++) {
+            s1_count[s1.charAt(i) -'a']++;
+            s2_count[s2.charAt(i) -'a']++;
+        }
+
+        int matches = 0;
+        for(int i=0; i<s1_count.length; i++) {
+            matches += (s1_count[i] == s2_count[i]) ? 1 : 0;
+        }
+
+        int left = 0;
+        // read from where we left off above
+        for(int i=s1.length(); i<s2.length(); i++) {
+            if(matches == 26) {
+                return true; // means all characters count matched
+            }
+
+            int rightCharIndex = s2.charAt(i) -'a';
+            s2_count[rightCharIndex]++;
+            // after adding the next character, check if count at the index between s1_count and s2_count changed ?
+            if(s1_count[rightCharIndex] == s2_count[rightCharIndex]) {
+                matches++;
+            } else if(s1_count[rightCharIndex] +1 == s2_count[rightCharIndex]) {
+                matches--;
+            }
+
+            // since we have added right element, remove the left element from count and re-check
+            int leftCharIndex = s2.charAt(left) - 'a';
+            s2_count[leftCharIndex]--;
+            if(s1_count[leftCharIndex] == s2_count[leftCharIndex]) {
+                matches++;
+            } else if(s1_count[leftCharIndex] - 1 == s2_count[leftCharIndex]) {
+                matches--;
+            }
+            left++;
+        }
+
+        return matches == 26; // in case, the match happened at the last character.
+    }
+
     public static void main(String[] args) {
         System.out.println("################  Using HashMaps #######################");
         System.out.println(checkInclusion("ab","eidbaooo"));
@@ -96,5 +150,10 @@ public class PermutationInString {
         System.out.println("################  Using temp arrays with length 26 #######################");
         System.out.println(checkInclusionUsingArrays("ab","eidbaooo"));
         System.out.println(checkInclusionUsingArrays("ab","eidboaoo"));
+
+        System.out.println("################  Using temp arrays with length 26 and matchCount as we loop through characters #######################");
+        System.out.println(checkInclusionUsingArraysAndMatchCount("ab","eidbaooo"));
+        System.out.println(checkInclusionUsingArraysAndMatchCount("ab","eidboaoo"));
+
     }
 }
